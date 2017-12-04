0ddc344b6199e92c46c63717361526833272f634
Martin Krutak <devklingac@gmail.com>
2017-12-04 18:07:38 +0100

    running docker without params

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 docker kill lighthouse
-docker run -i -t -d -P --rm --name lighthouse -v "$PWD/app:/home/node/app" -v "$PWD/output:/home/node/data" lighthouse ./server.js -i input.json -o ../data/queue
+docker run -i -t -d -P --rm --name lighthouse -v "$PWD/app/input.json:/home/node/app/input.json" -v "$PWD/output:/home/node/data" lighthouse ./server.js -i input.json -o ../data/queue
+#docker run -i -t -d -P --rm --name lighthouse -v "$PWD/app/input.json:/home/node/app/input.json" -v "$PWD/output:/home/node/data" lighthouse
