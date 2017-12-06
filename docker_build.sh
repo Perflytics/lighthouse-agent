2b7c1100cfb4954f5bdd3cd51990856baadfb8fe
Martin Krutak <devklingac@gmail.com>
2017-12-06 17:34:37 +0100

    Remove --no-cache from building script

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --no-cache -f Dockerfile -t lighthouse . 
+docker build -f Dockerfile -t lighthouse .
 #docker run -d -p 8080:8080 --rm --name lighthouse -v "$PWD/app:/home/node/app" -v "$PWD/output:/home/node/data" lighthouse yarn install --production
