d33f7a464c98d2e85a58f0442a625dc371d8d910
Martin Krutak <devklingac@gmail.com>
2017-12-06 13:56:02 +0100

    Add lockfile during execution and imporvements in logging

--- app/server.js
+++ app/server.js
@@ -8,7 +8,12 @@ const chromeLauncher = require('chrome-launcher');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const debug = require('debug')('perflytics');
+const argparse = require('yargs-parser');
+const winston = require('winston');
+const logDirName = 'log';
+const env = process.env.NODE_ENV !== 'production';
 
+//initialization,config
 const perfConfig = require('lighthouse/lighthouse-core/config/perf.json');
 const DEFAULT_LIGHTHOUSE_OPTIONS = {logLevel: 'silent', output: 'json'};
 const DEFAULT_CHROME_FLAGS = ['--headless', '--disable-gpu', '--no-sandbox'];
@@ -18,10 +23,32 @@ var argv = require('yargs')
     .usage('Run lighthouse configured by JSON file')
     .example('$0', 'Run lighthouse script')
     .alias('o', 'output-dir').describe('o', 'Output directory')
+    .default('o', arg => arg ? arg : __dirname)
     .alias('v', 'verbose').describe('v', 'Verbose output')
     .required('i', 'Input file must be provided').alias('i', 'input file').describe('i', 'Input file with tests definitions')
     .help('h').alias('h', 'help')
     .argv;
+
+//setup logging
+const outputDir = argv.o;
+const logDir = outputDir+'/'+logDirName;
+const tsFormat = () => (new Date()).toLocaleTimeString();
+mkdirp(logDir);    //create logdir
+const logger = new (winston.Logger)({
+    transports: [
+        // colorize the output to the console
+        new (winston.transports.Console)({
+            timestamp: tsFormat,
+            colorize: true,
+            level: 'info'
+        }),
+        new (winston.transports.File)({
+            filename: `${logDir}/results.log`,
+            timestamp: tsFormat,
+            level: env === 'development' ? 'debug' : 'info'
+        })
+    ]
+});
 
 async function launchChrome(flags = {}){
     return chromeLauncher.launch({chromeFlags: flags.chromeFlags});
@@ -42,84 +69,92 @@ async function registerLighthouseListener(event, reportStatusLog) {
 }
 
 async function createReportDir(reportDir) {
-    mkdirp(reportDir, (err) => {
+    logger.info('Storing files to %s', reportDir);
+    mkdirp.sync(reportDir, (err) => {
         if (err) {
-            console.error('Cannot create directory ', reportDir);
-            console.error(err);
-        }
-        else {
-            debug('Storing files to %s', reportDir);
+            logger.error('Cannot create directory ', reportDir);
+            logger.error(err);
         }
     });
 }
 
-async function main() {
-    debug('Inputfile provided is %s', argv.i);
-    var outputDir;
+function writeResultsToFile(resultFile, results) {
+    fs.writeFileSync(resultFile, results, (err) => {
+        if(err) {
+            logger.error('Cannot write report to dile ', resultFile);
+            logger.error(err);
+        }
+    });
+    logger.info('The result was saved to %s', resultFile);
+}
 
-    if(argv.o) {
-        outputDir = argv.o;
+async function createLockFile(dirname) {
+    var fd = fs.openSync(dirname+'/.lock', 'a');
+    return fd;
+}
+
+function deleteLockFile(dirname) {
+    fs.unlinkSync(dirname+'/.lock');
+    logger.info('deleting lockfile');
+}
+
+async function processTargets(reportOptions, reportDir, lighthouseOptions) {
+    for (let target of reportOptions.targets) {
+        let resultFile = reportDir + '/' + target.reportPageID + '.json';
+        let reportStatusLog = reportDir + '/' + target.reportPageID + '.status';
+        let reportWarnLog = reportDir + '/' + target.reportPageID + '.warn';
+
+        try {
+            let statusStream = registerLighthouseListener('status', reportStatusLog);
+            let warnStream = registerLighthouseListener('warning', reportWarnLog);
+
+            let results = await lighthouse(target.url, lighthouseOptions);
+
+            writeResultsToFile(resultFile, JSON.stringify(results));
+        } catch (e) {
+            logger.error(e);
+        }
     }
-    else {
-        outputDir = __dirname;
-    }
-    debug('Output will be placed to %s', outputDir);
+}
+
+
+async function main() {
+    logger.debug('Inputfile provided is %s', argv.i);
+
+    logger.debug('Output will be placed to %s', outputDir);
 
     //parsing input file
     let reportOptions = JSON.parse(fs.readFileSync(argv.i, 'utf8'));
     let reportID = reportOptions.reportID;
-    let chromeFlags = [...DEFAULT_CHROME_FLAGS, ...reportOptions.config.chromeFlags]
-    let lighthouseOptions = Object.assign(DEFAULT_LIGHTHOUSE_OPTIONS, reportOptions);
-    delete lighthouseOptions.config.chromeFlags;
-
-    debug('Lighhouse options', lighthouseOptions);
+    var chromeFlags = [...DEFAULT_CHROME_FLAGS, ...reportOptions.config.chromeFlags];
+    var lighthouseOptions = Object.assign(DEFAULT_LIGHTHOUSE_OPTIONS, argparse(reportOptions.config.options.join(' ')));
 
     try {
         let chrome = await launchChrome({chromeFlags: chromeFlags});
         lighthouseOptions.port = chrome.port;
-        debug('Started chrome with debug port on %s', chrome.port);
+        logger.info('Started chrome with debug port on %s', chrome.port);
 
         let reportDir = outputDir+'/'+reportID;
-        await createReportDir(reportDir);
 
-        for (let target of reportOptions.targets) {
+        createReportDir(reportDir).then(() => {
+            return createLockFile(reportDir)
+        });
 
-            let resultFile = reportDir+'/'+target.reportPageID+'.json';
-            let reportStatusLog = reportDir+'/'+target.reportPageID+'.status.log';
-            let reportWarnLog = reportDir+'/'+target.reportPageID+'.warn.log';
-
-            try {
-                let statusStream = registerLighthouseListener('status',reportStatusLog);
-                let warnStream = registerLighthouseListener('warning',reportWarnLog);
-
-                let results = await lighthouse(target.url, lighthouseOptions, perfConfig);
-
-                //@todo ne takto, ale radsi odchytavat results z LH pomoci streamu a ten pak zapisovat
-                fs.writeFile(resultFile, JSON.stringify(results), (err) => {
-                    if(err) {
-                        console.error('Cannot write report to dile ', resultFile);
-                        console.error(err);
-                    }
-                    else {
-                        debug('The result was saved to %s', resultFile);
-                    }
-                })
-
-                //@todo pridat soubor .done
-
-            } catch(e) {
-                console.error(e);
-            }
-        }
+        await processTargets(reportOptions, reportDir, lighthouseOptions);
 
         chrome.kill();
+
+        deleteLockFile(reportDir);
+
     } catch(e) {
-        console.error(e);
+        logger.error(e);
     }
 }
 
 module.exports = main;
 if(require.main == module) {
-    debug('Starting app');
-    main();
+    logger.info('Starting app');
+    main().then(() => {
+        logger.info('Done');
+    });
 }
