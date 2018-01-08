00b2688f1feb8a69f6138dfda958a18e3d5806c0
Martin Krutak <devklingac@gmail.com>
2018-01-08 12:06:52 +0100

    Adjust shm size for docker container

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker kill lighthouse
-#docker run -i -t -d -P --rm --name lighthouse -v "$PWD/data/input.json:/home/node/input.json" -v "$PWD/data:/home/node/data" lighthouse ./app/server.js -i ./input.json -o ./data
-docker run -i -t -d -P --rm --name lighthouse -v "$PWD/data/input.json:/home/node/input.json" -v "$PWD/data:/home/node/data" lighthouse
-#docker run -i -t -d -P --rm --name lighthouse -v "$PWD/app/input.json:/home/node/app/input.json" -v "$PWD/output:/home/node/data" lighthouse
+#docker run -i -t -d -P --rm --shm-size="128m" --name lighthouse -v "$PWD/data/input.json:/home/node/input.json" -v "$PWD/data:/home/node/data" lighthouse ./app/server.js -i ./input.json -o ./data
+docker run -i -t -d -P --rm --shm-size="128m" --name lighthouse -v "$PWD/input.json:/home/node/data/input.json" -v "$PWD/data:/home/node/data" lighthouse
+#docker run -i -t -d -P --rm --shm-size="128m" --name lighthouse -v "$PWD/input.json:/home/node/data/input.json" -v "$PWD/data:/home/node/data" -v "$PWD/node_modules:/home/node/node_modules" lighthouse
+#docker run -i -t -d -P --rm --shm-size="128m" --name lighthouse -v "$PWD/app/input.json:/home/node/app/input.json" -v "$PWD/output:/home/node/data" lighthouse
